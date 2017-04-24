b307047062141fbd7cffd30da3572c4cdff4c07d
David Hessing <delemach@gmail.com>
2017-04-24 17:09:10 +0000

    bug fix and formatting

--- modules/var_utils.py
+++ modules/var_utils.py
@@ -2,17 +2,19 @@ from lend_bid import LendBid
 
 def compact_lends_book(lends):
     new_lends = []
-    current_rate = lends[0].apr
+    current_rate = float(lends[0].rate)
+    current_apr = lends[0].apr
     current_amount = float(lends[0].amount)
     current_period = lends[0].period
     for lend in lends[1:]:
-        if current_period == lend.period and current_rate > 0.001 and current_rate - float(lend.apr) < 0.01:
+        if current_period == lend.period and current_apr > 0.001 and current_apr - float(lend.apr) < 0.01:
             current_amount += float(lend.amount)
         else:
             #note this relies on the fact that all LendBids in the list being compacted utilize the same rate_type and fee...
             new_lends.append(LendBid(rate=current_rate, amount=current_amount, period=current_period, rate_type=lend.rate_type, fee=lend.fee))
             current_amount = float(lend.amount)
-            current_rate = lend.apr
+            current_rate = float(lend.rate)
+            current_apr = lend.apr
             current_period = lend.period
     return new_lends
 
